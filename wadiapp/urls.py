db371c1c90b42bbbb6977bffeeb5511c67eb3763
krevion <ngujirironn@gmail.com>
2026-06-11 21:43:42 +0300

    Prepare for GitHub push; add .gitignore and clean generated files

--- wadiapp/urls.py
+++ wadiapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
--- wadiapp/views.py
+++ wadiapp/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product
 
 
 def home(request):
-	return render(request, 'home.html')
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
 
 
 def about(request):
-	return render(request, 'about.html')
+    return render(request, 'about.html')
 
 
 def contact(request):
-	return render(request, 'contactus.html')
+    return render(request, 'contactus.html')
